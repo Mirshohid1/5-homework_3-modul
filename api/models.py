6d46e88d7837602887a2ffe992ed04b26fda54e5
l0n3r <mirshohid1214@gmail.com>
2024-12-04 23:08:52 +0500

    models.py edited.

--- api/models.py
+++ api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
-from django.core.exceptions import ValidationError
-from datetime import date
+from .mixins import DueDateValidationMixin
 
-class Todo(models.Model):
+
+class Todo(models.Model, DueDateValidationMixin):
     title = models.CharField(
         max_length=255,
-        validators=MinLengthValidator(3, "The description must contain at least 12 characters")
+        validators=MinLengthValidator(3, "The description must contain at least 3 characters")
     )
     description = models.TextField(
         validators=MinLengthValidator(12, "The description must contain at least 12 characters")
@@ -17,13 +17,12 @@ class Todo(models.Model):
     def clean(self):
         """
         Validation and Data Processing.
+        due_date validation with the False flag (for the model)
         """
 
         self.title = self.title.strip().title()
         self.description = self.description.strip()
-
-        if self.due_date < date.today():
-            raise ValidationError({'due_date': "The value of due_date cannot be earlier than the current time"})
+        self.due_date = self.validate_due_date_mixin_method(self.due_date)
 
     def __str__(self):
         return f"Todo {self.title}: -> ({self.due_date})"
